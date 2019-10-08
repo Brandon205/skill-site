d31a2fcb478b591a2f813c13680f196adef135fc
Brandon205 <brandonblack02@gmail.com>
2019-10-07 17:46:21 -0700

    Deliverable done

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/contact', function(req, res) {
-    res.render('contact', {});
+    res.render('contact', {email: 'brandonblack02@gmail.com', twitter: 'https://twitter.com/brandonblack02', linkedIn: 'https://www.linkedin.com/in/brandon-b-757706b3/'});
 });
 
 app.get('/skills', function(req, res) {
-    res.render('skills', {});
-})
+    res.render('skills', {skills: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express']});
+});
 
 app.listen(3000);
